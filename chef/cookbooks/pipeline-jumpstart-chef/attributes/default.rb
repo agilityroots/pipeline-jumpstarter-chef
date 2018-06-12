8f04cacf41f91ea3032fce00f354ebf766b9b633
Anadi Misra <anadi.misra@agilityroots.com>
2018-06-12 13:48:32 +0530

    updated chef resources

--- chef/cookbooks/pipeline-jumpstart-chef/attributes/default.rb
+++ chef/cookbooks/pipeline-jumpstart-chef/attributes/default.rb
@@ -1,4 +1,10 @@
 # Java Attributes
-default['java']['jdk_version'] = '8'
-default['java']['install_flavor'] = 'oracle'
-default['java']['oracle']['accept_oracle_download_terms'] = true
+override['java']['jdk_version'] = '8'
+override['java']['install_flavor'] = 'oracle'
+override['java']['oracle']['accept_oracle_download_terms'] = true
+# Nexus Attributes
+override['nexus3']['url'] = 'http://download.sonatype.com/nexus/3/nexus-3.12.1-01-unix.tar.gz'
+override['nexus3']['properties_variables'] = { host: '0.0.0.0', port: '8081', args: '${jetty.etc}/jetty.xml,${jetty.etc}/jetty-http.xml,${jetty.etc}/jetty-requestlog.xml', context_path: '/nexus/' }
+# Jenkins Attributes
+override['jenkins']['master']['install_method'] = 'package'
+override['jenkins']['master']['jvm_options'] = '-Djenkins.install.runSetupWizard=false'
